015f33ea9cfddb4d751675458fdb802815367e08
EmanuelCottarel <emanuelcottarel@gmail.com>
2023-09-28 16:37:50 +0200

    possibilité d'enregistrer un arrosage depuis la liste des plantes

--- src/app/plant-list/plant-list.page.ts
+++ src/app/plant-list/plant-list.page.ts
@@ -1,11 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
-import {AlertController, IonicModule, ModalController} from '@ionic/angular';
+import {ActionSheetController, AlertController, IonicModule, ModalController} from '@ionic/angular';
 import {PlantService} from "../_services/plant.service";
 import {Plant} from "../_interfaces/plant";
 import {RouterLink} from "@angular/router";
 import {AddPlantModalComponent} from "../_modales/add-plant-modal/add-plant-modal.component";
+import {DateService} from "../_services/date.service";
+import {ToastService} from "../_services/toast.service";
 
 @Component({
   selector: 'app-plant-list',
@@ -19,7 +21,10 @@ export class PlantListPage implements OnInit {
   constructor(
     private readonly plantService: PlantService,
     private readonly alertController: AlertController,
-    private readonly modalController: ModalController) {
+    private readonly modalController: ModalController,
+    private readonly actionSheetController: ActionSheetController,
+    private readonly dateService: DateService,
+    private readonly toastService: ToastService) {
   }
 
   plantList!: Plant[]
@@ -48,9 +53,39 @@ export class PlantListPage implements OnInit {
     return await alert.present();
   }
 
+   async presentActionSheet(plant: Plant) {
+    const actionSheet = await this.actionSheetController.create({
+      header: plant.name,
+      buttons: [
+        {
+          text: 'Arroser',
+          icon: 'water',
+          role: 'destructive',
+          handler: () => {
+            this.waterPlant(plant)
+          }
+        },
+        {
+          text: 'Annuler',
+          icon: 'close',
+          role: 'cancel'
+        },
+      ]
+    });
+    await actionSheet.present();
+  }
+
   deletePlant(plantId: number) {
    this.plantService.deletePlant(plantId);
    this.plantList = this.plantService.getAll();
+  }
+
+   waterPlant(plant: Plant) {
+    const today = new Date();
+    plant.lastWatering = this.dateService.formatDateToString(today);
+
+    this.plantService.updatePlant(plant);
+    this.toastService.showSuccesToast('La plante a bien été arrosé');
   }
 
   async openModal() {
